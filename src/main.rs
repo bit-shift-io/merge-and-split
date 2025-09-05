d55e80af45ce691fdf9f89fe4be60ac8017b58fb
Fabian Mathews <fabian@jpmedia.com.au>
2025-09-05 19:39:23 +0930

    Added initial particle and test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+#![allow(dead_code, unused_variables, unused_imports)]
+#![feature(test)]
+
 use merge_and_split::run;
 
 mod sort;
 mod introduction;
+mod particle2;
 
 fn main() {
     //run().unwrap();
